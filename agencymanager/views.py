9d1dc110fda067826c947d2773c59c56d67fceeb
Ngô Phước Thịnh <ngophuocthinh28@gmail.com>
2022-05-27 13:18:49 +0700

    .

--- agencymanager/views.py
+++ agencymanager/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from rest_framework import viewsets
 
+@login_required(login_url='login')
 def index(request):
     if request.method == 'GET':
         # <view logic>
-        return render(request, 'index.html')
+        return render(request, '1-tiepnhandaily.html')
