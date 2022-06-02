1390576541381280ad4f914d1501acf9e7abbb46
anhkhoalongha <52558199+anhkhoalongha@users.noreply.github.com>
2022-06-02 16:25:35 +0700

    Danh sach dai ly-template: TiepNhan

--- agencymanager/views.py
+++ agencymanager/views.py
@@ -6,8 +6,8 @@ from django.views.generic import TemplateView
 from rest_framework import viewsets
 
 
-from agencyapi.forms import ThuTienForm, TiepNhan
-from agencyapi.models import LoaiDaiLy
+from agencyapi.forms import ThuTienForm, TiepNhan, PhieuThuTien
+from agencyapi.models import DaiLy, LoaiDaiLy, PhieuThuTien
 
 
 @login_required(login_url='login')
@@ -18,19 +18,23 @@ def index(request):
 
 @login_required(login_url='login')
 def tiepnhan(request):    
+    daily_obj = DaiLy.objects.all()
+    context= {"daily": daily_obj}
     if request.method == 'POST':
         form=TiepNhan(request.POST)
         if form.is_valid():
             print(form.data)
             form.save()            
-            return render(request, '1-tiepnhandaily.html')
-    return render(request, '1-tiepnhandaily.html')
+            return render(request, '1-tiepnhandaily.html', context)
+    
+    return render(request, '1-tiepnhandaily.html', context)
 
 @login_required(login_url='login')
 def nhaphang(request):
     if request.method == 'POST':
         # <view logic>
-        return render(request, '2-lapphieunhaphang.html')
+        pass
+    return render(request, '2-lapphieunhaphang.html')
 
 
 @login_required(login_url='login')
@@ -45,8 +49,13 @@ def thutien(request):
     if request.method == 'POST':
         form= ThuTienForm(request.POST)
         if form.is_valid():
-            print('TRUE')        
+            print('TRUE')
+            daily= DaiLy.objects.get()
+            a= PhieuThuTien(MaDaiLy = form.data['TenDaiLy'], ngayThuTien = form.data['NgayThuTien'], SoTienThu = form.data['SoTienThu'])
+            a.save()
             return render(request, '4-lapphieuthutien.html')
+            
+            
     return render(request, '4-lapphieuthutien.html')
 
 
